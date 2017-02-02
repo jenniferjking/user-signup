870ef88a805da25dd16115e53dc951329f249ab1
Jennifer King <jenniferjking@hotmail.com>
2017-02-02 14:07:42 -0600

    Finish error and redirect code

--- main.py
+++ main.py
@@ -40,37 +40,100 @@ page_footer = """
 </html>
 """
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        signup_form = """
+def create_form(username, error_username, error_password, error_verify, email, error_email):
+    signup_form = '''
         <form method="post">
             <table>
                 <tr>
                     <td><label for="username">Username: </label></td>
-                    <td><input name="username" type="text"></td>
+                    <td><input name="username" type="text" value="{0}"></td>
+                    <td class="error">{1}</td>
                 </tr>
                 <tr>
                     <td><label for="password">Password: </label></td>
-                    <td><input name="password" type="text"></td>
+                    <td><input name="password" type="password"></td>
+                    <td class="error">{2}</td>
                 </tr>
                 <tr>
                     <td><label for="verify_password">Verify Password: </label></td>
-                    <td><input name="verify_password" type="text"></td>
+                    <td><input name="verify_password" type="password"></td>
+                    <td class="error">{3}</td>
                 </tr>
                 <tr>
                     <td><label for="email">Email (optional): </label></td>
-                    <td><input name="email" type="text"></td>
+                    <td><input name="email" type="text" value="{4}"></td>
+                    <td class="error">{5}</td>
                 </tr>
             </table>
-            <input type="submit" value="Submit">
-        </form>
-        """
+            <input type="submit">
+        </form>'''.format(username, error_username, error_password, error_verify, email, error_email)
+    return signup_form
 
+
+
+user_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and user_re.match(username)
+
+pass_re = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and pass_re.match(password)
+
+email_re = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return email and email_re.match(email)
+
+
+class Signup(webapp2.RequestHandler):
+    
+    def get(self):
+
+        signup_form = create_form("","","","","","")
         content = page_header + signup_form + page_footer
         self.response.write(content)
 
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify_password = self.request.get('verify_password')
+        email = self.request.get('email')
 
+        error_username = ""
+        error_password= ""
+        error_verify= ""
+        error_email= ""
+
+        if not valid_username(username):
+            error_username = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            error_password = "That's not a valid password."
+            have_error = True
+        elif password != verify_password:
+            error_verify = "Your passwords didn't match."
+            have_error = True
+
+        if email:    
+            if not valid_email(email):
+                error_email = "That's not a valid email."
+                have_error = True
+
+        if have_error:
+            signup_form = create_form(username, error_username, error_password, error_verify, email, error_email)
+            content = page_header + signup_form + page_footer
+            self.response.write(content)
+        else:
+            self.redirect('/welcome?username=' + username)
+
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username')
+        content = page_header + "<h2> Welcome, " + username + "!</h2>" + page_footer
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Signup),
+    ('/welcome', Welcome)
 ], debug=True)
